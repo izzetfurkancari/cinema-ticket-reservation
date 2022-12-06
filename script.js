0a385dd6a0eece60d73bfbe68bfcbe2486651fe2
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-06 21:30:52 +0100

    js localstorage added

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount")
 const select = document.querySelector("#movie")
 const seats = document.querySelectorAll(".seat:not(.reserved)")
 
+getFromLocalStorage();
+calculateTotal();
+
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
         e.target.classList.toggle("selected")
@@ -40,6 +43,25 @@ function calculateTotal() {
     amount.innerText = selectedSeatCount * select.value
 
     saveToLocalStorage(selectedSeatIndex)
+}
+
+function getFromLocalStorage(){
+    const selectedSeats = JSON.stringify(localStorage.getItem("selectedSeats"))
+
+    if(selectedSeats != null && selectedSeats.length > 0) {
+        seats.forEach(function(seats, index) {
+            if (selectedSeats.indexOf(index) > -1){
+                seats.classList.add("selected")
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex")
+
+    if (selectedMovieIndex != null) {
+        select.selectedIndex = selectedMovieIndex;
+
+    }
 }
 
 function saveToLocalStorage(index) {
