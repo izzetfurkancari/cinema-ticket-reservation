923fe417b24b5dd8bb4430209818adee97c2f29c
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-06 21:16:42 +0100

    js localstorage started

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const container = document.querySelector('.container');
 const count = document.getElementById("count");
 const amount = document.getElementById("amount")
 const select = document.querySelector("#movie")
+const seats = document.querySelectorAll(".seat:not(.reserved)")
 
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
@@ -15,8 +16,33 @@ select.addEventListener("change", (e) => {
 })
 
 function calculateTotal() {
-    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
-    let price = select.value;
+    const selectedSeats = container.querySelectorAll(".seat.selected")
+
+    const selectedSeatsArr = [];
+    const seatsArr =[];
+
+    selectedSeats.forEach((seat)=>{
+        selectedSeatsArr.push(seat)
+    })
+
+    seats.forEach((seat)=>{
+        seatsArr.push(seat);
+    });
+
+    let selectedSeatIndex = selectedSeatsArr.map((seat)=>{
+        return seatsArr.indexOf(seat)
+    } )
+
+    console.log(selectedSeatIndex);
+
+    let selectedSeatCount = selectedSeats.length;
     count.innerText = selectedSeatCount;
-    amount.innerText = selectedSeatCount * price
+    amount.innerText = selectedSeatCount * select.value
+
+    saveToLocalStorage(selectedSeatIndex)
+}
+
+function saveToLocalStorage(index) {
+    localStorage.setItem("selectedSeats", JSON.stringify(index))
+    localStorage.setItem("selectedMovieIndex", select.selectedIndex)
 }
