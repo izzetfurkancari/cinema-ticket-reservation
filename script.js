62894624de87589b296e61d7bc2eefdffdbedd46
izzetfurkancari <99566878+izzetfurkancari@users.noreply.github.com>
2022-12-06 20:54:01 +0100

    js seats calculated

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const container = document.querySelector('.container');
- 
+const count = document.getElementById("count");
+const amount = document.getElementById("amount")
+const select = document.querySelector("#movie")
+
 container.addEventListener('click', (e) => {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("reserved")){
         e.target.classList.toggle("selected")
+        calculateTotal()
     }
 })
+
+select.addEventListener("change", (e) => {
+    calculateTotal()
+})
+
+function calculateTotal() {
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length;
+    let price = select.value;
+    count.innerText = selectedSeatCount;
+    amount.innerText = selectedSeatCount * price
+}
